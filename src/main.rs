0290be1e05fcefa8fa05524c2aa10146350f9bf2
Anthony Mason <anthony81799@gmail.com>
2022-02-10 16:13:09 -0500

    My Crate

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+//! Documents the crate instead of the function
+
+/// Documentation comment
+/// 
+/// These use markdown for formatting and generate html
+/// 
+/// Common sections include Examples, Panics, Errors, and Safety
+/// 
+/// Any code blocks in the comment are run as tests with cargo test
+/// 
+fn main() {
+    println!("Hello, world!");
+}
