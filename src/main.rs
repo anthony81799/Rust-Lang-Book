8b7d335b8d6172199536e76854ae6455ba3978b1
Anthony Mason <anthony81799@gmail.com>
2022-02-10 15:50:54 -0500

    Concurrency

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+use std::{thread, time::Duration, sync::mpsc};
+mod mutex_examples;
+
+fn main() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+
+    let v = vec![1, 2, 3];
+
+    let handle2 = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle2.join().unwrap();
+
+    let (tx, rx) = mpsc::channel();
+
+    let tx2 = tx.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread")
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("messages"),
+            String::from("for"),
+            String::from("you")
+        ];
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got {}", received);
+    }
+
+    mutex_examples::run();
+}
